e141c7cd1156d34c253ae2ae644d95ec33277e50
Md. Nasim Uddin <md.nasim1552@gmail.com>
2023-07-29 04:32:52 +0600

    29-July-2023

--- PTLRealERP/Pages/Index.cshtml.cs
+++ PTLRealERP/Pages/Index.cshtml.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Dapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RealERPLIB.DapperRepository;
+using System.Data;
 
 namespace PTLRealERP.Pages
 {
@@ -9,19 +12,47 @@ namespace PTLRealERP.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IDapperService _dapperService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IDapperService dapperService)
         {
             _logger = logger;
+            _dapperService = dapperService;
         }
+        
 
         public void OnGet()
         {
-            int x = 10;
-            if (x == 5)
-            {
+           this.GetModulename();
+            this.GetInterfacename();
+        }
 
-            }
+        private void GetModulename()
+        {            
+            string comcod = "3101";
+            string procedureName = "SP_UTILITY_LOGIN_MGT";
+            string Calltype = "GETCOMMODULE";
+            string userid = "3101001";
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@Comp1", comcod);
+            parameters.Add("@Calltype", Calltype);
+            parameters.Add("@Desc1", userid);
+            List<List<dynamic>> listOfModule = _dapperService.GetDataList(procedureName, parameters);
+            ViewData["ModuleNames"] = listOfModule[0];
+        }
+
+        private void GetInterfacename()
+        {
+            string comcod = "3101";
+            string procedureName = "SP_UTILITY_LOGIN_MGT";
+            string Calltype = "GETINTERFACE";
+            string userid = "3101001";
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@Comp1", comcod);
+            parameters.Add("@Calltype", Calltype);
+            parameters.Add("@Desc1", userid);
+            List<List<dynamic>> listOfModule = _dapperService.GetDataList(procedureName, parameters);
+            ViewData["InterfaceNames"] = listOfModule[0];
         }
     }
 }
