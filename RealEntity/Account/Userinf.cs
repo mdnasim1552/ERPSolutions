67dbea39944cf1d537e2ffcaaa8ea4640852348d
Md. Nasim Uddin <md.nasim1552@gmail.com>
2023-09-01 07:18:43 +0600

    01-Sep-2023

---
+++ RealEntity/Account/Userinf.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEntity.Account
+{
+    public class Userinf
+    {
+        public Userinf()
+        {
+            //select comcod,usrid,usrsname,usrname,usrdesig,usractive,usrpass,mailid,empid,userrole from userinf
+        }
+        public string comcod { get; set; }
+        public string usrid { get; set; }
+        public string usrsname { get; set; }
+        public string usrname { get; set;}
+        public string usrdesig { get; set; }
+        public bool usractive { get; set; }
+        public string usrpass { get; set; }
+        public string mailid { get; set; }
+        public string empid { get; set; }
+        public string userrole { get; set; }
+
+
+    }
+}
